cf771cdcaff5319860fb4d0c5d58fc1cf7dbb79b
Sinetheta <k.attfield@gmail.com>
2016-06-13 20:06:50 -0700

    Remove unused secondary nav from footer
    Short open tags is an option which can result in tears
    
    http://php.net/manual/en/ini.core.php#ini.short-open-tag
    
    Here it will print some "commented out" code onto the page for visitors
    if this theme is used inthe wrong environment.

--- footer.php
+++ footer.php
@@ -2,11 +2,10 @@
 <div class="push"></div>
 </div><!--end pusher-->
 
-<footer id="colophon" class="cf"> 
+<footer id="colophon" class="cf">
 	<div class="footer_wrapper">
-        <?/*php wp_nav_menu ( array( 'theme_location' => 'secondary-menu', 'menu_class' => 'text_menu cf', ) );*/ ?>
-        <?php wp_nav_menu ( array( 'theme_location' => 'secondary-menu', 'menu_class' => 'pic_menu cf', ) ); ?>
-    </div>
+    <?php wp_nav_menu ( array( 'theme_location' => 'secondary-menu', 'menu_class' => 'pic_menu cf', ) ); ?>
+  </div>
 </footer>
 
 
