28d757b16e3c8488689bc4c536e7e835869e7e68
Romanulus <roman.olariu@gmail.com>
2016-06-15 15:25:17 -0700

    Add hero-boxes to homepage and rnd bg image
    Add three hero-boxes to the homepage along with the random homepage
    backgrounds pulled from specifically tagged posts.

--- page.php
+++ page.php
@@ -23,8 +23,48 @@
 
 <?php
 if ( is_home() || is_front_page() ) { ?>
-    <?php echo "<div id='page_push'></div>";
-} else { ?>
+    <?php echo "<div id='page_push'></div>";?>
+
+    <div class="bgFeatureBox">
+        <p>
+        <?php
+        $args = array( 'posts_per_page' => 1, 'orderby' => 'rand', 'tag' => 'frontpage' );
+        $myposts = get_posts( $args );
+        foreach ( $myposts as $post ) : setup_postdata( $post );?>
+            <a id="bgFeature" href="<?php the_permalink(); ?>">
+                <?php the_title();
+                $src = wp_get_attachment_image_src( get_post_thumbnail_id($post_id), 'full' )[0];
+                //echo "<img style='padding:0 0 0 5px;' align='right' src='". $src ."' />";
+                ?>
+            </a><br />
+            <?php echo("<style type='text/css'>body {background-image: url('".$src."') !important}</style>");?>
+            <span class="bgFeatureLocation">
+            <?php
+            echo(get_post_meta( get_the_ID(), 'Location', true ));
+            ?></span>
+        <?php endforeach;
+        wp_reset_postdata();?>
+        </>
+    </div>
+    <div class="heroboxes">
+        <div class="herobox">
+            <h1>about windley</h1>
+            <p>Praesent non consequat elit. Nam convallis mauris at tellus semper, sed pulvinar felis condimentum.  Praesent non consequat elit. Fusce sed maximus orci. Mauris tristique ligula a erat pellentesque tincidunt. </p>
+            <a href="#" class="herobutton">LEARN MORE</a>
+        </div>
+        <div class="herobox">
+            <h1>community stuff</h1>
+            <p>Donec posuere cursus sapien. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Suspendisse potenti. Pellentesque sed ipsum elit.</p>
+            <a href="#" class="herobutton">LEARN MORE</a>
+        </div>
+        <div class="herobox">
+            <h1>third column</h1>
+            <p>Cras eu nisi lorem. Sed consectetur arcu ut lectus sollicitudin suscipit vel nec nisl. Vestibulum dapibus scelerisque velit, sed tincidunt mauris ullamcorper quis. Sed mollis tincidunt venenatis.</p>
+            <a href="#" class="herobutton">LEARN MORE</a>
+        </div>
+    </div>
+
+<?php } else { ?>
 
 <div id="page" class="wrapper cf fixer_wrapper">
 
