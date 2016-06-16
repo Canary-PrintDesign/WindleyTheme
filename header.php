b65b82f8d1da18d34a964a513fdf9fd5fa96d415
Sinetheta <k.attfield@gmail.com>
2016-06-15 22:48:07 -0700

    Add fonts

--- header.php
+++ header.php
@@ -7,11 +7,12 @@
 <link href="<?php echo get_stylesheet_directory_uri();?>/style.css" rel="stylesheet" type="text/css">
 <link href="<?php echo get_stylesheet_directory_uri();?>/build/css/main.css" rel="stylesheet" type="text/css">
 <link href="<?php echo get_stylesheet_directory_uri();?>/images/favicon.png" rel="icon" type="image/png">
+<link href='https://fonts.googleapis.com/css?family=Open+Sans:600,700,800|Merriweather:700italic,400' rel='stylesheet' type='text/css'>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <!--jquery-->
 <script src="<?php echo get_template_directory_uri(); ?>/js/modernizr.js"></script>
-<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>  
-<?php wp_head(); ?> 
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+<?php wp_head(); ?>
 <script>
 $(document).ready(function() {
 $('#wprmenu_bar').click(function() {
