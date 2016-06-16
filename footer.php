d6bca9c9addbf8299db8ec7ba3a08e1a6ed02275
Sinetheta <k.attfield@gmail.com>
2016-06-15 22:56:45 -0700

    Add contact info to footer

--- footer.php
+++ footer.php
@@ -4,6 +4,19 @@
 
 <footer class="page-footer">
   <div class="container">
+    <div class="contact-info">
+      <h4>Windey Contracting Ltd.</h4>
+      <ul class="address-list">
+        <li>3711 Shenton Road</li>
+        <li>Nanaimo, B.C.</li>
+        <li>V9T 2H1Canada</li>
+      </ul>
+
+      <ul class="address-list">
+        <li>250-758-3411</li>
+        <li><a href="mailto:info@windleycontracting.com"><i class="fa fa-envelope fa-lg" aria-hidden="true"></i> info@windleycontracting.com</a></li>
+      </ul>
+    </div>
   </div>
 </footer>
 
