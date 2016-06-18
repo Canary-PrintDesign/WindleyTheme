e18143d7bbc332b4dee0ec358712ba09452e8825
Sinetheta <k.attfield@gmail.com>
2016-06-18 10:44:21 -0700

    Extract topnav into partial

--- header.php
+++ header.php
@@ -27,12 +27,5 @@ $('#wprmenu_bar').click(function() {
 </script>
 </head>
 <body <?php body_class(); ?>>
-<div id="header_wrapper" class="cf">
-	<header id="masthead" class="wrapper cf">
-		<a href="<?php echo home_url(); ?>" title="Windley Contracting - Home" class="home_link"><img src="<?php echo get_template_directory_uri(); ?>/images/windley-logo.png" alt="Windley Contracting logo" class="logo"></a>
-	<nav id="mainmenu">
-	<?php wp_nav_menu ( array( 'theme_location' => 'primary-menu' ) ); ?>
-	</nav>
-	</header>
-</div><!--end header_wrapper-->
-    <div id="pageup"></div>
+  <?php include('_topnav.php'); ?>
+  <div id="pageup"></div>
