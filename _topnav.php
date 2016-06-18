2e04e620796056a11072614cbe15ee96b3024695
Sinetheta <k.attfield@gmail.com>
2016-06-18 11:36:46 -0700

    Burn old topnav

--- _topnav.php
+++ _topnav.php
@@ -1,8 +1,5 @@
-<div id="header_wrapper" class="cf">
-  <header id="masthead" class="wrapper cf">
-    <a href="<?php echo home_url(); ?>" title="Windley Contracting - Home" class="home_link"><img src="<?php echo get_template_directory_uri(); ?>/images/windley-logo.png" alt="Windley Contracting logo" class="logo"></a>
-    <nav id="mainmenu">
-      <?php wp_nav_menu ( array( 'theme_location' => 'primary-menu' ) ); ?>
-    </nav>
-  </header>
-</div>
+<nav class="topnav">
+  <div class="nav-wrapper">
+    <?php wp_nav_menu ( array( 'theme_location' => 'primary-menu' ) ); ?>
+  </div>
+</nav>
