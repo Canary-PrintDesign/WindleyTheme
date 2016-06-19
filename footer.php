d4b7bc279341da5bfaebf30ac8281d8b11815276
adamish717 <adam@piechnik.ca>
2016-06-19 00:27:58 -0700

    Update footer

--- footer.php
+++ footer.php
@@ -3,15 +3,15 @@
 <footer class="page-footer">
   <div class="container">
     <div class="contact-info">
-      <h4>Windey Contracting Ltd.</h4>
+      <h4>Windley Contracting Ltd.</h4>
       <ul class="address-list">
-        <li>3711 Shenton Road</li>
-        <li>Nanaimo, B.C.</li>
-        <li>V9T 2H1Canada</li>
+        <li>3711 Shenton Road</li>
+        <li>Nanaimo, B.C. V9T 2H1</li>
+        <li>Canada</li>
       </ul>
-
+      <br>
       <ul class="address-list">
-        <li>250-758-3411</li>
+        <li><a href="tel:+1 250-758-3411"><i class="fa fa-phone fa-lg" aria-hidden="true"></i> 250-758-3411</a></li>
         <li><a href="mailto:info@windleycontracting.com"><i class="fa fa-envelope fa-lg" aria-hidden="true"></i> info@windleycontracting.com</a></li>
       </ul>
     </div>
@@ -30,12 +30,19 @@
     <div class="social-links">
       <div class="social-highlight">
         <h4>Follow us</h4>
-        <a href="https://www.instagram.com/windleycontracting/"><i class="fa fa-instagram fa-lg" aria-hidden="true"></i></a>
-        <a href="https://twitter.com/WindleyHQ"><i class="fa fa-twitter fa-lg" aria-hidden="true"></i></a>
-        <a href="https://www.facebook.com/windleycontracting"><i class="fa fa-facebook fa-lg" aria-hidden="true"></i></a>
+        <a href="https://www.instagram.com/windleycontracting/"><i class="fa fa-instagram fa-lg social-icons" aria-hidden="true"></i></a>
+        <a href="https://twitter.com/WindleyHQ"><i class="fa fa-twitter fa-lg social-icons" aria-hidden="true"></i></a>
+        <a href="https://www.facebook.com/windleycontracting"><i class="fa fa-facebook fa-lg social-icons" aria-hidden="true"></i></a>
       </div>
+    </div>
+  </div>
+  <div class="footer-bottom">
+    <div class="container">
       <div class="credits">
         Proudly presented by <a href="https://www.canaryprint.ca/">Canary Print+Design</a>
+      </div>
+      <div class="copyright">
+        &copy <?php echo date("Y") ?> Windley Contracting LTD.  All rights reserved
       </div>
     </div>
   </div>
