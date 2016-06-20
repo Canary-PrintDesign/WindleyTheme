49f5629326e92edb4a845c46f3ac191e40bf8d5e
Sinetheta <k.attfield@gmail.com>
2016-06-19 19:13:47 -0700

    Wrap topnav components in container

--- _topnav.php
+++ _topnav.php
@@ -1,7 +1,10 @@
 <nav class="topnav">
-  <div class="nav-wrapper">
-    <?php wp_nav_menu ( array( 'theme_location' => 'topnav-left' ) ); ?>
-    <?php the_custom_logo(); ?>
-    <?php wp_nav_menu ( array( 'theme_location' => 'topnav-right' ) ); ?>
+  <div class="container">
+    <div class="nav-wrapper">
+      <?php wp_nav_menu ( array( 'theme_location' => 'topnav-left' ) ); ?>
+      <?php the_custom_logo(); ?>
+      <?php wp_nav_menu ( array( 'theme_location' => 'topnav-right' ) ); ?>
+    </div>
   </div>
 </nav>
+<!-- <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a> -->
