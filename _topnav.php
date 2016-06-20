1c6f7c29c2af7d271d4e19145675da1a59ffa0d2
Sinetheta <k.attfield@gmail.com>
2016-06-19 17:12:04 -0700

    Split nav into left and right section
    It's hard to center a logo, this is the best of a few bad options.

--- _topnav.php
+++ _topnav.php
@@ -1,7 +1,7 @@
 <nav class="topnav">
   <div class="nav-wrapper">
+    <?php wp_nav_menu ( array( 'theme_location' => 'topnav-left' ) ); ?>
     <?php the_custom_logo(); ?>
-
-    <?php wp_nav_menu ( array( 'theme_location' => 'primary-menu' ) ); ?>
+    <?php wp_nav_menu ( array( 'theme_location' => 'topnav-right' ) ); ?>
   </div>
 </nav>
--- functions.php
+++ functions.php
@@ -9,8 +9,8 @@
 
     register_nav_menus(
         array(
-        'primary-menu' => 'Primary Menu',
-        'secondary-menu' => 'Secondary Menu',
+        'topnav-left'   => 'Main Menu - Left Side',
+        'topnav-right'  => 'Main Menu - Right Side',
         'footer-menu-1' => 'Footer Menu 1',
         'footer-menu-2' => 'Footer Menu 2'
         )
