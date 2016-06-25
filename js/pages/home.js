dc8d7622eebf220e0980b3a914a71ed3446651cf
Sinetheta <k.attfield@gmail.com>
2016-06-25 12:48:42 -0700

    Prevent flash of bg blur on page load
    Since we were waiting for doc ready to do our scroll check, but using
    "reverse" styles (eg: :not(.at-scroll-top)) there was a moment in time
    where the bg was visible, and blurred, before we swooped in and removed
    the blur.
    
    Although the classname "scrolled-past-top" isn't quite as logically
    clear, it's more useful for our purpose. And we avoid jumping thourgh
    hoops with a "doc-ready" class.

--- js/pages/home.js
+++ js/pages/home.js
@@ -1,10 +1,9 @@
 function onScroll() {
-  $('body').toggleClass('at-scroll-top', $(window).scrollTop() === 0);
+  $('body').toggleClass('past-scroll-top', $(window).scrollTop() > 0);
 }
 
 $(window).on('scroll', onScroll);
 
 $(function() {
-  $(document.body).addClass('doc-ready');
   onScroll();
 });
