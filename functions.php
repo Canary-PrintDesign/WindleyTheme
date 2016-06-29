cf76654d649340e07de8ad0c246e6086c05a90e4
adamish717 <adam@piechnik.ca>
2016-06-28 22:57:02 -0700

    Add "Project Location" Meta-Box to Posts in admin

--- functions.php
+++ functions.php
@@ -23,4 +23,58 @@ function menu_name($location){
   echo $footer_nav_1->name;
 }
 
+
+// Add Custom Meta-Box called "Project Location"
+
+// Function is called via: echo(get_post_meta( get_the_ID(), 'location', true ));
+
+/**
+ * Adds a meta box to the post editing screen
+ */
+function prfx_custom_meta() {
+    add_meta_box( 'prfx_meta', __( 'Project Location', 'prfx-textdomain' ), 'prfx_meta_callback', 'post' );
+}
+add_action( 'add_meta_boxes', 'prfx_custom_meta' );
+
+
+/**
+ * Outputs the content of the meta box
+ */
+function prfx_meta_callback( $post ) {
+    wp_nonce_field( basename( __FILE__ ), 'prfx_nonce' );
+    $prfx_stored_meta = get_post_meta( $post->ID );
+    ?>
+
+    <p>
+        <label for="meta-text" class="prfx-row-title"><?php _e( 'Location:', 'prfx-textdomain' )?></label>
+        <input type="text" name="meta-text" id="meta-text" value="<?php if ( isset ( $prfx_stored_meta['location'] ) ) echo $prfx_stored_meta['location'][0]; ?>" />
+    </p>
+
+    <?php
+}
+
+/**
+ * Saves the custom meta input
+ */
+function prfx_meta_save( $post_id ) {
+
+    // Checks save status
+    $is_autosave = wp_is_post_autosave( $post_id );
+    $is_revision = wp_is_post_revision( $post_id );
+    $is_valid_nonce = ( isset( $_POST[ 'prfx_nonce' ] ) && wp_verify_nonce( $_POST[ 'prfx_nonce' ], basename( __FILE__ ) ) ) ? 'true' : 'false';
+
+    // Exits script depending on save status
+    if ( $is_autosave || $is_revision || !$is_valid_nonce ) {
+        return;
+    }
+
+    // Checks for input and sanitizes/saves if needed
+    if( isset( $_POST[ 'location' ] ) ) {
+        update_post_meta( $post_id, 'location', sanitize_text_field( $_POST[ 'location' ] ) );
+    }
+
+}
+add_action( 'save_post', 'prfx_meta_save' );
+// --END "Project Location" Meta-Box--
+
 ?>
