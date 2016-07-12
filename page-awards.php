e3ad8405e4b8970193775884dd77b0aa0e37a203
adamish717 <adam@piechnik.ca>
2016-07-12 16:55:21 -0700

    Add Awards page

---
+++ page-awards.php
@@ -0,0 +1,158 @@
+<?php get_template_part( '/partials/header' ); ?>
+<?php get_template_part( '/partials/hero' ); ?>
+
+
+<div class="container">
+  <div class="industry-awards">
+    <h2>Industry Awards</h2>
+
+    <h3>2015</h3>
+    <ul><span class="organization">VIREB</span>
+      <li>
+        <span class="category">Community Institutional Award</span> | <span class="award">Award of Excellence</span><br>
+        <a href="#">Uplands Walk</a>
+      </li>
+      <li>
+        <span class="category">Retail</span> | <span class="award">Award of Excellence</span><br>
+        <a href="#">Island Optimal</a>
+      </li>
+      <li>
+        <span class="category">Multi Family Apartment</span> | <span class="award">Award of Excellence</span><br>
+        <a href="#">Songbird</a>
+      </li>
+      <li>
+        <span class="category">Office</span> | <span class="award">Honorable Mention</span><br>
+        <a href="#">1825 Bowen Road</a>
+      </li>
+    </ul>
+    <ul><span class="organization">Green Nanaimo</span>
+      <li>
+        <span class="category">Commercial Development</span> | <span class="award">Design Award</span><br>
+        <a href="#">Island Optimal</a>
+      </li>
+      <li>
+        <span class="category">Public Institution Development</span> | <span class="award">Design Award</span><br>
+        <a href="#">Uplands Walk Supportive Housing</a>
+      </li>
+    </ul>
+
+    <h3>2014</h3>
+    <ul><span class="organization">VIREB</span>
+      <li>
+        <span class="category">Hospitality Award</span> | <span class="award">Award of Excellence</span><br>
+        <a href="#">Nanaimo Golf Club</a>
+      </li>
+      <li>
+        <span class="category">Multi-Family Apartment Award</span> | <span class="award">Award of Excellence</span><br>
+        <a href="#">Madrona Green</a>
+      </li>
+      <li>
+        <span class="category">Commercial Building Award All Categories</span> | <span class="award">Award of Excellence</span><br>
+        <a href="#">Nanaimo Golf Club</a>
+      </li>
+      <li>
+        <span class="category">Industrial Building</span> | <span class="award">Honorable Mention</span><br>
+        <a href="#">Gregg Distributors</a>
+      </li>
+    </ul>
+
+    <h3>2013</h3>
+    <ul><span class="organization">VIREB</span>
+      <li>
+        <span class="category">Commercial Building Award</span> | <span class="award">Award of Excellence</span><br>
+        <a href="#">City Hall Annex</a>
+      </li>
+      </ul>
+      <ul><span class="organization">MOTI</span>
+      <li>
+        <span class="category">Deputy Minister Contractor of the Year Award in Grading</span> | <span class="award">1st Place</span><br>
+        <a href="#">Highway 97</a>
+      </li>
+    </ul>
+
+    <h3>2012</h3>
+    <ul><span class="organization">VIREB</span>
+      <li>
+        <span class="category">Industrial Renovation Award</span> | <span class="award">Award of Excellence</span><br>
+        <a href="#">Boban Drive City Tile</a>
+      </li>
+    </ul>
+
+    <h3>2011</h3>
+    <ul><span class="organization">VIREB</span>
+      <li>
+        <span class="category">Multi-Purpose Building Award</span> | <span class="award">Award of Merit</span><br>
+        <a href="#">1650 Terminal Avenue</a>
+      </li>
+    </ul>
+
+    <h3>2009</h3>
+    <ul><span class="organization">VIREB</span>
+      <li>
+        <span class="category">Commercial Building Award</span> | <span class="award">Award of Excellence</span><br>
+        <a href="#">Wallace Street Project</a>
+      </li>
+    </ul>
+
+    <h3>2008</h3>
+    <ul><span class="organization">VIREB</span>
+      <li>
+        <span class="category">Commercial Building Award</span> | <span class="award">Award of Merit</span><br>
+        <a href="#">VI Fitness</a>
+      </li>
+    </ul>
+    <ul><span class="organization">City of Nanaimo</span>
+      <li>
+        <span class="category">Commercial</span> | <span class="award">Design Award</span><br>
+        <a href="#">Wallace Street Project</a>
+      </li>
+    </ul>
+
+    <h3>2004</h3>
+    <ul><span class="organization">City of Nanaimo</span>
+      <li>
+        <span class="category">Commercial</span> | <span class="award">Design Award</span><br>
+        <a href="#">Herold Engineering & Windley Contracting Buildings - Shenton Road</a>
+      </li>
+    </ul>
+
+    <h3>2003</h3>
+    <ul><span class="organization">City of Nanaimo</span>
+      <li>
+        <span class="category">Multi-Family</span> | <span class="award">Honourable Mention</span><br>
+        <a href="#">6140 McGirr Road</a>
+      </li>
+    </ul>
+  </div>
+
+  <div class="leed">
+    <h2>LEED Certification</h2>
+    <h3>Gold</h3>
+    <li>
+      <a href="#">Uplands Walk</a>
+    </li>
+    <li>
+      <a href="#">Ladysmith Community Centre</a>
+    </li>
+    <li>
+      <a href="#">RDN Church Road Transfer Station</a>
+    </li>
+    <li>
+      <a href="#">Family Place Parkvsille</a>
+    </li>
+    <h3>Silver</h3>
+    <li>
+      <a href="#">125 Wallace Street</a>
+    </li>
+    <li>
+      <a href="#">RDN Administrative and Transit Building</a>
+    </li>
+    <h3>Certified</h3>
+    <li>
+      <a href="#">Vancouver Island University - Faculty of Management</a>
+    </li>
+  </div>
+</div>
+
+
+<?php get_template_part( '/partials/footer' ); ?>
