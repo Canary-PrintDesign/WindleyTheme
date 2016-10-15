250a7102c3398c164fd3c0a7fa91c35700fc0f00
adamish717 <adam@piechnik.ca>
2016-10-15 15:02:07 -0700

    Add larger favicon

--- partials/header.php
+++ partials/header.php
@@ -8,6 +8,7 @@
 <!--css & favicon-->
 <link href="<?php echo get_stylesheet_directory_uri();?>/build/css/main.css" rel="stylesheet" type="text/css">
 <link href="<?php echo get_stylesheet_directory_uri();?>/images/favicon.png" rel="icon" type="image/png">
+<link href="<?php echo get_stylesheet_directory_uri();?>/images/favicon-large.png" sizes="192x192" rel="icon" type="image/png">
 <link href='https://fonts.googleapis.com/css?family=Open+Sans:600,700,800|Merriweather:700italic,400' rel='stylesheet' type='text/css'>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <?php wp_head(); ?>
