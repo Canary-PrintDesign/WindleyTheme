6ed2897c5e18bfcc88276421b9ab3bccadef90e7
adamish717 <adam@piechnik.ca>
2016-10-16 15:02:37 -0700

    Add Favicon Suite
    browserconfig.xml needs to be located in web root

--- partials/header.php
+++ partials/header.php
@@ -1,14 +1,24 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<meta name="theme-color" content="#B5A36A" />
+<!--favicon-->
+<link rel="apple-touch-icon" sizes="180x180" href="<?php echo get_stylesheet_directory_uri();?>/images/favicon/apple-touch-icon.png">
+<link rel="icon" type="image/png" href="<?php echo get_stylesheet_directory_uri();?>/images/favicon/favicon-32x32.png" sizes="32x32">
+<link rel="icon" type="image/png" href="<?php echo get_stylesheet_directory_uri();?>/images/favicon/favicon-16x16.png" sizes="16x16">
+<link rel="icon" type="image/png" href="<?php echo get_stylesheet_directory_uri();?>/images/favicon/favicon-192x192.png" sizes="192x192">
+<link rel="manifest" href="<?php echo get_stylesheet_directory_uri();?>/images/favicon/manifest.json">
+<link rel="mask-icon" href="<?php echo get_stylesheet_directory_uri();?>/images/favicon/safari-pinned-tab.svg" color="#b5a36a">
+<meta name="apple-mobile-web-app-title" content="Windley Contracting LTD.">
+<meta name="application-name" content="Windley Contracting LTD.">
+<meta name="msapplication-TileColor" content="#b5a36a">
 <meta name="msapplication-navbutton-color" content="#B5A36A" />
+<meta name="msapplication-TileImage" content="<?php echo get_stylesheet_directory_uri();?>/images/favicon/mstile-144x144.png">
+<meta name="theme-color" content="#b5a36a" />
+
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <title><?php bloginfo( 'name' ); ?></title>
-<!--css & favicon-->
+<!--css-->
 <link href="<?php echo get_stylesheet_directory_uri();?>/build/css/main.css" rel="stylesheet" type="text/css">
-<link href="<?php echo get_stylesheet_directory_uri();?>/images/favicon.png" rel="icon" type="image/png">
-<link href="<?php echo get_stylesheet_directory_uri();?>/images/favicon-large.png" sizes="192x192" rel="icon" type="image/png">
 <link href='https://fonts.googleapis.com/css?family=Open+Sans:600,700,800|Merriweather:700italic,400' rel='stylesheet' type='text/css'>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <?php wp_head(); ?>
@@ -16,3 +26,6 @@
 <body <?php body_class(); ?>>
   <?php include('_topnav_mobile.php'); ?>
   <?php include('_topnav.php'); ?>
+
+
+
